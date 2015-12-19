07e766f2c033deb2c890fbd9b975c5aace2236c2
John Jullies <johnjullies@gmail.com>
2015-12-19 23:31:43 +0800

    mobile menu button working under construction

--- exam.js
+++ exam.js
@@ -1,1 +1,11 @@
-()();
+var menu = document.querySelector("#menu-mobile");
+menu.addEventListener("click", function(e) {
+	if (menu.className == "fa fa-bars") {
+		document.querySelector(".menu-hidden").style.display = "block";
+		menu.className = "fa fa-times";
+	}
+	else {
+		document.querySelector(".menu-hidden").style.display = "none";
+		menu.className = "fa fa-bars";
+	}
+});
